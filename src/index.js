cb2cf61c65fb65d29787d951cba08357629d6347
Rene Cruz <renecruz.0711@gmail.com>
2023-12-05 23:24:52 -0600

    Adding main routes

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route exac path="/" element={<Home />} />
+      </Routes>
+    </Router>
     <App />
   </React.StrictMode>
 );
